8d320d803ea6b97a8a5d579a0b1e370f38f37aa8
jaynikam2005 <jaynikam2005@gmail.com>
2026-01-12 12:29:02 +0530

    Day-1

---
+++ python-basics/Day-1 T-1.py
@@ -0,0 +1,12 @@
+#Print "Hello World" 
+greet ="Hello"
+name="nana"
+message=f"{greet} {name} Welcome"
+print (help(str) ) 
+
+
+
+'''Learned about 
+    Strings in Python
+    Methods associated with strings
+'''
